a7fe8fb1ec53238316220698d46f26884de17e66
Tonyjiafan <358982833@qq.com>
2017-08-04 16:41:25 +0800

    ➕getQueryString

--- public/js/test/test.js
+++ public/js/test/test.js
@@ -2,9 +2,11 @@ define(function(require,exports,module){
   //引入模块
   var $ = require('../base/jquery.min'),
       util = require('../util'),
-      reqAjax = util.reqAjax;
+      reqAjax = util.reqAjax,
+      getQueryString = util.getQueryString;
 
-  console.log('我是test');
+  // let type =  getQueryString('type');
+  console.log('test');
 
 
 }) //define闭合标记
