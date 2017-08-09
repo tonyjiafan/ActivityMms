24fbdfbd918082895d33a5ea11bed0076ef81ade
Tonyjiafan <358982833@qq.com>
2017-08-09 10:19:11 +0800

    参数传递

--- routes.js
+++ routes.js
@@ -3,11 +3,11 @@
 const activity_1and1 = './controler/1and1',
       index = require(`${activity_1and1}/index`),
       test = require(`./controler/test/test`),
-      test2 = require(`./controler/test/test2`);
+      testVideo = require(`./controler/test/test2`);
 
 
 exports.urls = function(app){
     index.url(app), //首页
     test.url(app), //测试
-    test2.url(app) //测试
+    testVideo.url(app) //测试
 }
